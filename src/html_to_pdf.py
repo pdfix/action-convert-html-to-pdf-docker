b3392734a73464b800f3588f87236600cc68707d
4gac <feri4gac@gmail.com>
2024-03-27 12:37:42 +0100

    feat: added version info to config.json

--- src/html_to_pdf.py
+++ src/html_to_pdf.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 import os
 import platform
 import subprocess
@@ -104,19 +105,46 @@ def convert_to_pdf(url: str, output: str):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--url", help="URL address or local path to HTML", required=True
-    )
-    parser.add_argument("--output", help="Output file", required=True)
-    parser.add_argument(
-        "--verbose", help="Print debug info", required=False, action="store_true"
-    )
+    parser.add_argument("--url", help="URL address or local path to HTML")
+    parser.add_argument("--output", help="Output file")
+    parser.add_argument("--verbose", help="Print debug info", action="store_true")
+    parser.add_argument("-v", "--version", help="Print version", action="store_true")
     args = parser.parse_args()
+
+    if args.version:
+        try:
+            config_path = "config.json"
+            with open(config_path, "r") as f:
+                data = json.load(f)
+
+                if "version" in data:
+                    version_info = data["version"]
+                    MAJOR = version_info.get("major")
+                    MINOR = version_info.get("minor")
+                    PATCH = version_info.get("patch")
+
+                print("Version: {}.{}.{}".format(MAJOR, MINOR, PATCH))
+            sys.exit(0)
+        except FileNotFoundError as err:
+            print(
+                "Failed to get version information. Missing {} file.".format(
+                    config_path
+                ),
+                file=sys.stderr,
+            )
+            sys.exit(1)
 
     if args.verbose:
         print("Running in verbose mode. Debug messages enabled.")
         global DEBUG
         DEBUG = True
+
+    if args.url is None:
+        print("Missing required argument --url")
+        sys.exit(1)
+    if args.output is None:
+        print("Missing required argument --output")
+        sys.exit(1)
 
     convert_to_pdf(args.url, args.output)
 
