f8445a65e606ec86e1e707e9022305ad043bbee1
Peter Kral <peter.kral@pdfix.net>
2025-06-04 10:01:46 +0200

    RUFF and MYPY fixes for tests folder. Requirements minimized. Added func docs. Removed unnecessary try except from creating variable. Added missing parameters. Added proper file handling during logging to file (close was not there added context manager instead)

--- tests/collect_urls.py
+++ tests/collect_urls.py
@@ -1,51 +1,69 @@
-import requests
-from bs4 import BeautifulSoup
+import sys
 from urllib.parse import urljoin
 
+import requests
+from bs4 import BeautifulSoup
 
-def get_links(url):
-    links = set()
+
+def get_links(url: str) -> set[str]:
+    """
+    Extracts from webpage all URLs.
+
+    Args:
+        url (str): Webpage URL.
+
+    Returns:
+        Set containing all URLs from webpage.
+    """
+    links: set[str] = set()
     try:
         response = requests.get(url, timeout=30)
         soup = BeautifulSoup(response.content, "html.parser")
         for a_tag in soup.find_all("a", href=True):
-            link = a_tag["href"]
-            # if "#" in link:
-            #     continue
+            link: str = a_tag["href"]
 
             if link.startswith("http"):  # Absolute URL
                 links.add(link)
             else:  # Relative URL
-                absolute_link = urljoin(url, link)
+                absolute_link: str = urljoin(url, link)
                 links.add(absolute_link)
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f"An error occurred: {e}", file=sys.stderr)
     return links
 
 
-def crawl_website(start_url, max_pages):
-    crawled_pages = set()
-    pages_to_crawl = {start_url}
+def crawl_website(start_url: str, max_pages: int) -> set[str]:
+    """
+    Starts website crawler at start_url and stops either when all
+    webpages are crawled or when max_pages limit is reached.
+
+    Args:
+        start_url (str): URL to start crawling from.
+        max_pages (int): Limit when crawler stops processing webpages.
+
+    Returns:
+        Set of webpage URLs (can be more then max_pages).
+    """
+    crawled_pages: set[str] = set()
+    pages_to_crawl: set[str] = {start_url}
 
     while pages_to_crawl and len(crawled_pages) < max_pages:
         url = pages_to_crawl.pop()
         if url not in crawled_pages:
-            print("Crawling:", url)
+            print(f"Crawling: {url}")
             crawled_pages.add(url)
-            new_links = get_links(url)
+            new_links: set[str] = get_links(url)
             pages_to_crawl.update(new_links - crawled_pages)
 
     return crawled_pages
 
 
 if __name__ == "__main__":
-    start_url = input("Enter the starting URL: ")
-    max_pages = int(input("Enter the maximum number of pages to crawl: "))
+    start_url: str = input("Enter the starting URL: ")
+    max_pages: int = int(input("Enter the maximum number of pages to crawl: "))
 
-    crawled_pages = crawl_website(start_url, max_pages)
+    crawled_pages: set[str] = crawl_website(start_url, max_pages)
 
-    # print("\nCrawled Pages:")i
-
-    with open("collected_links.txt", "w") as f:
+    with open("collected_links.txt", "w") as file:
         for page in crawled_pages:
-            f.write(page + "\n")
+            file.write(page + "\n")
--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,56 +1,76 @@
-import platform
+import datetime
+import multiprocessing
 import random
 import string
 import subprocess
 import sys
-from tqdm import tqdm
-import docker
-import multiprocessing
-import datetime
+from multiprocessing.pool import ThreadPool
 
+from tqdm import tqdm
 
 CHROME_VERSION = "122.0.6261.111"
 
 
 def run_chrome(url: str) -> bool:
-    try:
-        chrome = "../dist/html_to_pdf/html_to_pdf"
-    except Exception as e:
-        print("Chrome cli was not found.", file=sys.stderr)
-        return False
+    """
+    Run chrome to convert url webpage into pdf.
 
-    random_name = (
-        "".join(random.choices(string.ascii_lowercase + string.digits, k=10)) + ".pdf"
-    )
+    Args:
+        url (str): Webpage URL.
 
+    Returns:
+        True if converted, False otherwise.
+    """
+    chrome = "../dist/html_to_pdf/html_to_pdf"
+    random_name = "".join(random.choices(string.ascii_lowercase + string.digits, k=10)) + ".pdf"
     args = [chrome, "--url", url, "--output", random_name]
-    log_to_file("Running with args: " + " ".join(args))
+    log_to_file(f"Running with args: {' '.join(args)}")
 
-    result = subprocess.run(args, shell=False, capture_output=True, text=True)
+    result = subprocess.run(args, shell=False, capture_output=True, text=True, check=False)
 
     if result.returncode == 0:
         print("Command executed successfully")
         return True
     else:
-        print("Error:", result.stderr, file=sys.stderr)
+        print(f"Error: {result.stderr}", file=sys.stderr)
         return False
 
 
-def read_file_line_by_line_with_progress(file_path):
-    total = 0
-    ok = 0
+def get_number_of_lines(file_path: str) -> int:
+    """
+    Calculate number of lines in file.
+
+    Args:
+        file_path (str): path to file.
+
+    Returns:
+        Number of lines.
+    """
+    try:
+        with open(file_path, "r") as file:
+            return sum(1 for line in file)
+    except FileNotFoundError:
+        return 0
+
+
+def test_urls_from_file_with_chrome(file_path: str) -> None:
+    """
+    Takes all URLs from file. For each creates thread and try to open it using chrome.
+    Calculate how many succed.
+
+    Args:
+        file_path (str): Paht to file containing URLs on each line.
+    """
+    total: int = get_number_of_lines(file_path)
+    ok: int = 0
 
     try:
         with open(file_path, "r") as file:
-            from multiprocessing.pool import ThreadPool
-
             pool = ThreadPool(processes=1)
 
-            total = sum(1 for line in file)
-            file.seek(0)  # Reset file pointer to the beginning
             with tqdm(total=total, desc="Running tests", unit="test") as pbar:
-                for url in file:
-                    url = url.strip()
+                for line in file:
+                    url = line.strip()
 
                     async_result = pool.apply_async(run_chrome, (url,))
 
@@ -59,23 +79,28 @@ def read_file_line_by_line_with_progress(file_path):
                         if res:
                             ok += 1
                     except multiprocessing.context.TimeoutError:
-                        log_to_file("[Failed] Chrome timed out. {}".format(url))
+                        log_to_file(f"[Failed] Chrome timed out for: {url}")
 
                     pbar.update(1)
 
     except FileNotFoundError:
         print(f"File '{file_path}' not found.")
 
-    print("Summary:\nTotal tests (passed): {} ({})\n".format(total, ok))
+    print(f"Summary:\nTotal tests (passed): {total} ({ok})\n")
 
 
-def log_to_file(msg: str):
-    logf = open("test.log", "a")
-    now = datetime.datetime.now()
+def log_to_file(message: str) -> None:
+    """
+    Logging message with date into file.
 
-    logf.write("[{}] {}\n".format(now.time(), msg))
+    Args:
+        message (str): Message to log.
+    """
+    with open("test.log", "a") as log_file:
+        now = datetime.datetime.now()
+        log_file.write(f"[{now.time()}] {message}\n")
 
 
 if __name__ == "__main__":
     file_path = "collected_links.txt"
-    read_file_line_by_line_with_progress(file_path)
+    test_urls_from_file_with_chrome(file_path)
