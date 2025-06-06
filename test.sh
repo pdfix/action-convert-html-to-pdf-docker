9e920b4dbe6222525fa74ae315f751f8fba7d93b
Peter Kral <peter.kral@pdfix.net>
2025-06-06 11:20:59 +0200

    Test improvement: - added docker remove (no left images) and tag to image name - each docker run have --rm (no left containers) - removed -it from docker run (we do not need open stdout and input) - consistent spaces (4 as tab, no trailing space at EOL) - added Test number to info text to be consistent with rest of external action tests

--- test.sh
+++ test.sh
@@ -6,37 +6,39 @@ info() { echo -e "\033[1;35m$1\033[0m"; }
 # init
 pushd "$(dirname $0)" > /dev/null
 
-img="html-to-pdf"
+img="html-to-pdf:test"
 pltfm="--platform linux/amd64"
+tmp_dir=".test"
 
 docker build $pltfm --rm -t $img .
 
-tmp_dir=".test"
 if [ -d "$(pwd)/$tmp_dir" ]; then
-  rm -rf $(pwd)/$tmp_dir
+    rm -rf $(pwd)/$tmp_dir
 fi
 mkdir -p $(pwd)/$tmp_dir
 
-info "list files in cwd"
-docker run $pltfm -it -v $(pwd):/data -w /data --entrypoint ls $img
+info "List files in cwd"
+docker run --rm $pltfm -v $(pwd):/data -w /data --entrypoint ls $img
 
-info "show help"
-docker run $pltfm -it -v $(pwd):/data -w /data $img --help
+info "Test #01: Show help"
+docker run --rm $pltfm -v $(pwd):/data -w /data $img --help
 
-info "extract config"
-docker run $pltfm -it -v $(pwd):/data -w /data $img config -o $tmp_dir/config.json
+info "Test #02: Extract config"
+docker run --rm $pltfm -v $(pwd):/data -w /data $img config -o $tmp_dir/config.json
 if [ ! -f "$(pwd)/$tmp_dir/config.json" ]; then
-  echo "config.json not saved"
-  exit 1
+    echo "config.json not saved"
+    exit 1
 fi
 
-info "run html-to-pdf"
-docker run $pltfm -it -v $(pwd):/data -w /data $img html-to-pdf --url https://pdfix.net -o $tmp_dir/pdfix.net.pdf --verbose
+info "Test #03: Run html-to-pdf"
+docker run --rm $pltfm -v $(pwd):/data -w /data $img html-to-pdf --url https://pdfix.net -o $tmp_dir/pdfix.net.pdf --verbose
 if [ ! -f "$(pwd)/$tmp_dir/pdfix.net.pdf" ]; then
-  echo "html-to-pdf to pdf failed on https://pdfix.net"
-  exit 1
+    echo "html-to-pdf to pdf failed on https://pdfix.net"
+    exit 1
 fi
 
+info "Removing testing docker image"
+docker rmi $img
 
 popd
 
