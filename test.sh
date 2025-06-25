69ca101174ff546de7ab2296a1c2ca53335b2c75
Peter Kral <peter.kral@pdfix.net>
2025-06-25 17:20:48 +0200

    PVQ-3923 External action - add simple test to all - uniting with format of prebuild tests in other repos

--- test.sh
+++ test.sh
@@ -1,45 +1,77 @@
 #!/bin/bash
 
-# local docker test 
-info() { echo -e "\033[1;35m$1\033[0m"; }
+# This is local docker test during build and push action.
+
+# Colors for output into console
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+PURPLE='\033[0;35m'
+NC='\033[0m' # No Color
+
+# Function to print info messages
+info() { echo -e "${PURPLE}$1${NC}"; }
+
+# Function to print success messages
+success() { echo -e "${GREEN}$1${NC}"; }
+
+# Function to print error messages
+error() { echo -e "${RED}ERROR: $1${NC}"; }
 
 # init
 pushd "$(dirname $0)" > /dev/null
 
-img="html-to-pdf:test"
-pltfm="--platform linux/amd64"
-tmp_dir=".test"
+EXIT_STATUS=0
+DOCKER_IMAGE="convert-html-to-pdf:test"
+PLATFORM="--platform linux/amd64"
+TEMPORARY_DIRECTORY=".test"
 
-docker build $pltfm --rm -t $img .
+info "Building docker image..."
+docker build $PLATFORM --rm -t $DOCKER_IMAGE .
 
-if [ -d "$(pwd)/$tmp_dir" ]; then
-    rm -rf $(pwd)/$tmp_dir
+if [ -d "$(pwd)/$TEMPORARY_DIRECTORY" ]; then
+    rm -rf $(pwd)/$TEMPORARY_DIRECTORY
 fi
-mkdir -p $(pwd)/$tmp_dir
+mkdir -p $(pwd)/$TEMPORARY_DIRECTORY
 
 info "List files in cwd"
-docker run --rm $pltfm -v $(pwd):/data -w /data --entrypoint ls $img
+docker run --rm $PLATFORM -v $(pwd):/data -w /data --entrypoint ls $DOCKER_IMAGE
 
 info "Test #01: Show help"
-docker run --rm $pltfm -v $(pwd):/data -w /data $img --help
+docker run --rm $PLATFORM -v $(pwd):/data -w /data $DOCKER_IMAGE --help > /dev/null
+if [ $? -eq 0 ]; then
+    success "passed"
+else
+    error "Failed to run \"--help\" command"
+    EXIT_STATUS=1
+fi
 
 info "Test #02: Extract config"
-docker run --rm $pltfm -v $(pwd):/data -w /data $img config -o $tmp_dir/config.json
-if [ ! -f "$(pwd)/$tmp_dir/config.json" ]; then
-    echo "config.json not saved"
-    exit 1
+docker run --rm $PLATFORM -v $(pwd):/data -w /data $DOCKER_IMAGE config -o $TEMPORARY_DIRECTORY/config.json > /dev/null
+if [ -f "$(pwd)/$TEMPORARY_DIRECTORY/config.json" ]; then
+    success "passed"
+else
+    error "config.json not saved"
+    EXIT_STATUS=1
 fi
 
 info "Test #03: Run html-to-pdf"
-docker run --rm $pltfm -v $(pwd):/data -w /data $img html-to-pdf --url https://pdfix.net -o $tmp_dir/pdfix.net.pdf --verbose
-if [ ! -f "$(pwd)/$tmp_dir/pdfix.net.pdf" ]; then
-    echo "html-to-pdf to pdf failed on https://pdfix.net"
-    exit 1
+docker run --rm $PLATFORM -v $(pwd):/data -w /data $DOCKER_IMAGE html-to-pdf --url https://pdfix.net -o $TEMPORARY_DIRECTORY/pdfix.net.pdf --verbose > /dev/null
+if [ -f "$(pwd)/$TEMPORARY_DIRECTORY/pdfix.net.pdf" ]; then
+    success "passed"
+else
+    error "html-to-pdf to pdf failed on https://pdfix.net"
+    EXIT_STATUS=1
 fi
 
 info "Removing testing docker image"
-docker rmi $img
+docker rmi $DOCKER_IMAGE
 
-popd
+popd > /dev/null
 
-echo "SUCCESS"
+if [ $EXIT_STATUS -eq 1 ]; then
+    error "One or more tests failed."
+    exit 1
+else
+    success "All tests passed."
+    exit 0
+fi
