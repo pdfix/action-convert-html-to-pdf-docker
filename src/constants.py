e15f1fac7ec171305835ea5d43ac7a52a2bde31c
Peter Kral <peter.kral@pdfix.net>
2026-02-20 17:20:49 +0100

    fix(action,PVQ-4872): Chosen different docker API (that works and returns data) to retrieve latest tag.

--- src/constants.py
+++ src/constants.py
@@ -1,2 +1,4 @@
 CONFIG_FILE = "config.json"
-DOCKER_IMAGE = "pdfix/convert-html-to-pdf"
+DOCKER_NAMESPACE = "pdfix"
+DOCKER_REPOSITORY = "convert-html-to-pdf"
+DOCKER_IMAGE = f"{DOCKER_NAMESPACE}/{DOCKER_REPOSITORY}"
